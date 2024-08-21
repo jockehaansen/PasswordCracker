5b39f67bd53d11667dcf9238871d3e5a00b8cc56
Joakim Hansen <jocke.hansen@outlook.com>
2024-08-21 16:01:41 +0200

    first commit, dependencies and sdk. .env and application-properties setup

---
+++ src/test/java/com/example/javasecpasswordcracker/JavasecPasswordCrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.javasecpasswordcracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavasecPasswordCrackerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
