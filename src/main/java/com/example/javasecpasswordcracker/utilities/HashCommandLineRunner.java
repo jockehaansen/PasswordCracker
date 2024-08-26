e60a97b8df408c5b36f5706eb26828b2bfd89ed2
Joakim Hansen <jocke.hansen@outlook.com>
2024-08-26 15:41:00 +0200

    commandline runner done with profile setup for running the hashing.

---
+++ src/main/java/com/example/javasecpasswordcracker/utilities/HashCommandLineRunner.java
@@ -0,0 +1,44 @@
+package com.example.javasecpasswordcracker.utilities;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Component;
+import org.springframework.util.ResourceUtils;
+
+import java.io.*;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+@Component
+@Profile("HashRunner")
+public class HashCommandLineRunner implements CommandLineRunner {
+
+    private final Logger logger = Logger.getLogger(HashCommandLineRunner.class.getName());
+    private int sampleSize = 1000; //safety exit satt av sample size på lösenorden
+    @Override
+    public void run(String... args) throws IOException {
+        try {
+            File inputFile = ResourceUtils.getFile("classpath:static/example2.txt");
+            File outputFile = ResourceUtils.getFile("classpath:static/output2.txt");
+            readAndWriteHash(inputFile, outputFile);
+        } catch (Exception e){
+            logger.warning("Trouble finding files");
+        }
+    }
+
+    public void readAndWriteHash(File inputFile, File outputFile){
+        try (BufferedReader reader = new BufferedReader(new FileReader(inputFile));
+             FileWriter writer = new FileWriter(outputFile)) {
+
+            String currentLine;
+            while ((currentLine = reader.readLine()) != null || sampleSize < 0) {
+                String appendHash = " | MD5:" + HashUtil.md5(currentLine) + " | SHA256:" + HashUtil.sha256(currentLine);
+                currentLine += appendHash;
+                writer.write(currentLine + "\n");
+                sampleSize--; //safety exit satt av sample size på lösenorden
+            }
+        } catch (IOException e) {
+            logger.log(Level.SEVERE, "An IOException has occurred: ", e);
+        }
+    }
+}
