9b4ba6bb9b0937ff6c8af8ca7b30935d7b57460d
Joakim Hansen <jocke.hansen@outlook.com>
2024-08-27 19:16:27 +0200

    mvp done, working product

--- src/main/java/com/example/javasecpasswordcracker/controllers/CrackerController.java
+++ src/main/java/com/example/javasecpasswordcracker/controllers/CrackerController.java
@@ -1,11 +1,16 @@
 package com.example.javasecpasswordcracker.controllers;
 
+import com.example.javasecpasswordcracker.services.HashService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
 @Controller
 public class CrackerController {
+
+    @Autowired
+    private HashService hashService;
 
     @GetMapping("/crackstation")
     public String crackstationPage(){
@@ -14,8 +19,9 @@ public class CrackerController {
 
     @PostMapping("/crackstation/crackpassword")
     public String crackPassword(@RequestParam("input") String input, Model model){
-        //do things
-        model.addAttribute("result", input);
+        System.out.println(input);
+        hashService.crackHashAndReturnPassword(model, input);
+        System.out.println(model.getAttribute("result"));
         return "crackstation";
     }
 
